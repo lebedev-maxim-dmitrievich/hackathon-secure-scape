0c065990c83dd0029514f32138d26787fad3f529
MisSero <misha555835@gmail.com>
2024-03-24 21:55:16 +0300

    Добавил клиента для обращения к сервису пользователей

--- src/EduPlatform.TaskService/Clients/UserServiceClient.cs
+++ src/EduPlatform.TaskService/Clients/UserServiceClient.cs
@@ -1,22 +1,37 @@
-﻿using System.Net.Http;
+﻿using EduPlatform.TaskService.DTOs;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace EduPlatform.TaskService.Clients;
 
 public class UserServiceClient
 {
     private const string _baseUrl = "localhost";
-    private const int _port = 8080;
-    private const string _urlUpdateProgress = "";
+    private const int _port = 8081;
 
     private readonly HttpClient _httpClient;
+
+    private string _urlUpdateProgress => $"{_baseUrl}:{_port}/api/userprofile/";
 
     public UserServiceClient()
     {
         _httpClient = new HttpClient();
     }
 
-    public void SendUpdateProgress()
+    public async Task SendUpdateProgress(ProgressUpdateDto progressUpdate)
     {
+        var requestOptions = new HttpRequestMessage(HttpMethod.Put, _urlUpdateProgress);
 
+        requestOptions.Content = new StringContent(JsonSerializer.Serialize(progressUpdate));
+
+        var response = await _httpClient.SendAsync(requestOptions);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Получение статус код {response.StatusCode}" +
+                $"при обращении к {_urlUpdateProgress}");
+        }
     }
 }
