8f47809f83345ef91cbdd84de4d7f9851ff2569d
MisSero <misha555835@gmail.com>
2024-03-24 09:38:58 +0300

    Настроил сервис задач

---
+++ src/EduPlatform.TaskService/Auth/AuthOptions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+namespace EduPlatform.TaskService.Auth;
+
+public static class AuthOptions
+{
+    static AuthOptions()
+    {
+        string? envKey = Environment.GetEnvironmentVariable(_keyEnvName);
+        if (envKey == null)
+        {
+            throw new Exception($"В переменной окржения {_keyEnvName} не задано значение");
+        }
+        else
+        {
+            _key = envKey;
+        }
+    }
+
+    private static readonly string _key;
+    private const string _keyEnvName = "SECRET_KEY";
+
+    public const string Issuer = "UserService";
+    public const string Audience = "SecureScape";
+
+    public const string Salt = "2v042Sgaq1@%11t";
+
+    public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_key));
+}
--- src/EduPlatform.TaskService/Program.cs
+++ src/EduPlatform.TaskService/Program.cs
@@ -1,5 +1,12 @@
+using EduPlatform.TaskService.Auth;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace EduPlatform.TaskService
 {
@@ -11,9 +18,60 @@ namespace EduPlatform.TaskService
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = AuthOptions.Issuer,
+                    ValidateAudience = true,
+                    ValidAudience = AuthOptions.Audience,
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey()
+                };
+            });
+            builder.Services.AddAuthorization();
+
+            // Подключение к бд
+            //string? connectionString = Environment.GetEnvironmentVariable(_connectionStringEnvName);
+            //builder.Services.AddDbContext<DbContext, AppDbContext>(options =>
+            //    options.UseNpgsql(connectionString));
+
+            builder.Services.AddControllers();
+
+            // Добавление сваггера с использование xml-документирования
+            builder.Services.AddSwaggerGen(config =>
+            {
+                string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                config.IncludeXmlComments(xmlPath);
+            });
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            //// Проверка подключения к бд
+            //using (var scope = app.Services.CreateScope())
+            //{
+            //    DbContext dbContext = scope.ServiceProvider.GetRequiredService<DbContext>();
+            //    //dbContext.Database.EnsureDeleted();
+            //    dbContext.Database.EnsureCreated();
+            //}
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.MapGet("/", () => "Hello from task servie!");
 
             app.Run();
         }
