ed9856818fbae5472eeed7b8c3fe8f3dc98aefee
MisSero <misha555835@gmail.com>
2024-03-24 15:53:10 +0300

    Добавил сервис для обработки логики задач

---
+++ src/EduPlatform.TaskService/Services/ITaskService.cs
@@ -0,0 +1,11 @@
+﻿using EduPlatform.TaskService.DTOs;
+using System.Threading.Tasks;
+
+namespace EduPlatform.TaskService.Services;
+
+public interface ITaskService
+{
+    public Task<TaskVm> GetTaskById(int id);
+
+    public Task<TasksVm> GetTasks(string? topicName, string? difficultName);
+}
