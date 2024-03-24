216510f22cfa1f14fdda375b91833546fe8ccddc
MisSero <misha555835@gmail.com>
2024-03-24 15:53:36 +0300

    Обновил сущность задача

--- src/EduPlatform.TaskService/Entities/TaskEntity.cs
+++ src/EduPlatform.TaskService/Entities/TaskEntity.cs
@@ -5,13 +5,13 @@ namespace EduPlatform.TaskService.Entities;
 public class TaskEntity
 {
     public long Id { get; set; }
-    public string Exercise { get; set; } = string.Empty;
-    public string FileLocation { get; set; } = string.Empty;
-    public string IconLocation { get; set; } = string.Empty;
-    public string Answer { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public Difficulties Difficult { get; set; }
+    public string Exercise { get; set; } = string.Empty;
+    public string Answer { get; set; } = string.Empty;
+    public string FileLocation { get; set; } = string.Empty;
+    public string IconLocation { get; set; } = string.Empty;
+    public string Difficult { get; set; } = string.Empty;
     public int Points { get; set; }
 
     public Topic Topic { get; set; } = null!;
