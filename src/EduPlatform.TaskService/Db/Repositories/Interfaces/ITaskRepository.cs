31ad9d689c0acbf5d0d48c004646feed8e3cd7fc
MisSero <misha555835@gmail.com>
2024-03-24 15:47:36 +0300

    ДОбавил репозиторий для заданий

---
+++ src/EduPlatform.TaskService/Db/Repositories/Interfaces/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using EduPlatform.TaskService.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EduPlatform.TaskService.Db.Repositories.Interfaces;
+
+public interface ITaskRepository
+{
+    public Task<TaskEntity> GetTaskById(int id);
+
+    public Task<List<TaskEntity>> GetTasks(string? topicName,
+        string? difficultName);
+}
