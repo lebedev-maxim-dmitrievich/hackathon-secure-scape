fbd4629370adfcdf30668b453d3a67d129f6104d
MisSero <misha555835@gmail.com>
2024-03-24 15:52:33 +0300

    Добавил dto

---
+++ src/EduPlatform.TaskService/DTOs/TaskPresentationVm.cs
@@ -0,0 +1,21 @@
+﻿using EduPlatform.TaskService.Enums;
+
+namespace EduPlatform.TaskService.DTOs;
+public class TaskPresentationVm
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Difficult { get; set; }
+    public string IconLocation { get; set; }
+
+    public TaskPresentationVm(long id, string title, string desctiption,
+        string difficult, string iconLocation)
+    {
+        Id = id;
+        Title = title;
+        Description = desctiption;
+        Difficult = difficult;
+        IconLocation = iconLocation;
+    }
+}
---
+++ src/EduPlatform.TaskService/DTOs/TasksVm.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace EduPlatform.TaskService.DTOs;
+
+public class TasksVm
+{
+    List<TaskPresentationVm> Tasks { get; set; }
+
+    public TasksVm(List<TaskPresentationVm> tasks)
+    {
+        Tasks = tasks;
+    }
+}
